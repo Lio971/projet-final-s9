0120362782b648a705498fc60f80d9ab50369b59
Rémi <remilp@hotmail.com>
2019-09-05 14:31:45 +0200

    add tp to Gemfile & cart jointable

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,8 @@
 class Cart < ApplicationRecord
+    belongs_to :user 
+    has_many :items 
+# do something so that user can't buy an item several times on the same cart
+
+#in item model : has_many :carts 
+
 end
--- db/migrate/20190905113033_create_carts.rb
+++ db/migrate/20190905113033_create_carts.rb
@@ -1,7 +1,11 @@
 class CreateCarts < ActiveRecord::Migration[5.2]
   def change
     create_table :carts do |t|
-
+      t.belongs_to :user, foreign_key: true
+      t.integer  "quantity",   default: 1
+      t.integer  "product_id"
+      t.integer  "order_id"
+         validates :quantity, numericality { :equal_to: 1 }
       t.timestamps
     end
   end

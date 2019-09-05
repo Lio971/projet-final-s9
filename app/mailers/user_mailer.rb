ca640b52c979d901f2b09a38de63292e549e7371
Rémi <remilp@hotmail.com>
2019-09-05 15:15:51 +0200

    correct comment

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
     default from: ENV['MAILJET_SENDER']
-    #ça sera là qu'il faudra faire la méthode du mail à envoyer après paiement
+#after purchase e-mail for customer
     def purchase_email(user)
         @user = user 
         mail(to:@user.email, subject: 'Votre paiement a été accepté !')

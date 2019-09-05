cccbf17f10fc801848f084adab7e870bff36c351
Lio971 <l.roques971@gmail.com>
2019-09-05 22:05:44 +0200

    ajout d'un peu de front, malheureusement pas abouti, il reste à connecter les bonnes routes et appeler les bonnes partials)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  root to: 'home#index'
   devise_for :users
   resources :users
+  resources :cart_items
+  resources :carts
+  resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

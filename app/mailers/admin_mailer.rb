e6e9c4f4993b09da711180bf10aef98062b5a0de
Rémi <remilp@hotmail.com>
2019-09-05 15:19:08 +0200

    admin_mailer method

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,2 +1,8 @@
 class AdminMailer < ApplicationMailer
+    default from: ENV['MAILJET_SENDER']
+# email after purchase for admin to inform order
+    def purchase_email(user)
+        @user = user 
+        mail(to:ENV['MAILJET_SENDER'], subject: 'Une commande à préparer a été effectué !')
+    end
 end

600c3323901ecff0d3db5b75761259b30892f478
Rémi <remilp@hotmail.com>
2019-09-05 14:38:38 +0200

    delete one comment

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,7 +1,6 @@
 class Cart < ApplicationRecord
     belongs_to :user 
     has_many :items 
-# do something so that user can't buy an item several times on the same cart
 
 #in item model : has_many :carts 
 
